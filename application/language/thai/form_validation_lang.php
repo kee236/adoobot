127fe9d46c32b75bb2e77ca80a8355a9a2a89472
kee236 <31869356+kee236@users.noreply.github.com>
2025-03-29 08:21:39 +0700

    Create form_validation_lang.php

---
+++ application/language/thai/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+$lang['is_exist']                        = "<b>%s</b> ไม่มีอยู่ในระบบ"; // User-defind (Created by Alamin)
+$lang['required']                        = "<b>%s</b> จำเป็นต้องระบุ";
+$lang['isset']                                = "<b>%s</b> ต้องมีค่า";
+$lang['valid_email']                = "<b>%s</b> ต้องมีรูปแบบอีเมลที่ถูกต้อง";
+$lang['valid_emails']                = "<b>%s</b> ต้องมีรูปแบบอีเมลที่ถูกต้องทั้งหมด";
+$lang['valid_url']                        = "<b>%s</b> ต้องมีรูปแบบ URL ที่ถูกต้อง";
+$lang['valid_ip']                        = "<b>%s</b> ต้องมีรูปแบบ IP ที่ถูกต้อง";
+$lang['min_length']                        = "<b>%s</b> ต้องมีความยาวอย่างน้อย <b>%s</b> ตัวอักษร";
+$lang['max_length']                        = "<b>%s</b> ต้องมีความยาวไม่เกิน <b>%s</b> ตัวอักษร";
+$lang['exact_length']                = "<b>%s</b> ต้องมีความยาว <b>%s</b> ตัวอักษรพอดี";
+$lang['alpha']                                = "<b>%s</b> สามารถประกอบด้วยตัวอักษรเท่านั้น";
+$lang['alpha_numeric']                = "<b>%s</b> สามารถประกอบด้วยตัวอักษรและตัวเลขเท่านั้น";
+$lang['alpha_dash']                        = "<b>%s</b> สามารถประกอบด้วยตัวอักษร, ตัวเลข, เครื่องหมายขีดล่าง, และเครื่องหมายขีดกลางเท่านั้น";
+$lang['numeric']                        = "<b>%s</b> ต้องประกอบด้วยตัวเลขเท่านั้น";
+$lang['is_numeric']                        = "<b>%s</b> ต้องประกอบด้วยตัวเลขเท่านั้น";
+$lang['integer']                        = "<b>%s</b> ต้องประกอบด้วยจำนวนเต็ม";
+$lang['regex_match']                = "<b>%s</b> มีรูปแบบไม่ถูกต้อง";
+$lang['matches']                        = "<b>%s</b> ไม่ตรงกับฟิลด์ <b>%s</b>";
+$lang['is_unique']                         = "<b>%s</b> ถูกใช้งานแล้ว";
+$lang['is_natural']                        = "<b>%s</b> ต้องประกอบด้วยตัวเลขบวกเท่านั้น";
+$lang['is_natural_no_zero']        = "<b>%s</b> ต้องประกอบด้วยตัวเลขที่มากกว่าศูนย์";
+$lang['decimal']                        = "<b>%s</b> ต้องประกอบด้วยเลขทศนิยม";
+$lang['less_than']                        = "<b>%s</b> ต้องมีค่าน้อยกว่า <b>%s</b>";
+$lang['greater_than']                = "<b>%s</b> ต้องมีค่ามากกว่า <b>%s</b>";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./application/language/thai/form_validation_lang.php */

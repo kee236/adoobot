dd26e60ce73eb7fc85c1a41a0bd613831a161239
kee236 <31869356+kee236@users.noreply.github.com>
2025-03-29 08:13:02 +0700

    Create thai/db_lang.php

---
+++ application/language/thai/db_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['db_invalid_connection_str'] = 'ไม่สามารถระบุการตั้งค่าฐานข้อมูลจากสตริงการเชื่อมต่อที่คุณส่งมาได้';
+$lang['db_unable_to_connect'] = 'ไม่สามารถเชื่อมต่อกับเซิร์ฟเวอร์ฐานข้อมูลโดยใช้การตั้งค่าที่ระบุได้';
+$lang['db_unable_to_select'] = 'ไม่สามารถเลือกฐานข้อมูลที่ระบุ: %s';
+$lang['db_unable_to_create'] = 'ไม่สามารถสร้างฐานข้อมูลที่ระบุ: %s';
+$lang['db_invalid_query'] = 'คำสั่ง SQL ที่คุณส่งมาไม่ถูกต้อง';
+$lang['db_must_set_table'] = 'คุณต้องระบุตารางฐานข้อมูลที่จะใช้กับคำสั่ง SQL ของคุณ';
+$lang['db_must_use_set'] = 'คุณต้องใช้วิธี "set" เพื่ออัปเดตข้อมูล';
+$lang['db_must_use_index'] = 'คุณต้องระบุดัชนี (index) สำหรับการอัปเดตแบบกลุ่ม (batch updates)';
+$lang['db_batch_missing_index'] = 'หนึ่งแถวหรือมากกว่าที่ส่งมาสำหรับการอัปเดตแบบกลุ่มขาดดัชนีที่ระบุ';
+$lang['db_must_use_where'] = 'ไม่อนุญาตให้อัปเดตข้อมูลหากไม่มีส่วน "where"';
+$lang['db_del_must_use_where'] = 'ไม่อนุญาตให้ลบข้อมูลหากไม่มีส่วน "where" หรือ "like"';
+$lang['db_field_param_missing'] = 'การดึงข้อมูลฟิลด์จำเป็นต้องระบุชื่อตารางเป็นพารามิเตอร์';
+$lang['db_unsupported_function'] = 'คุณสมบัตินี้ไม่พร้อมใช้งานสำหรับฐานข้อมูลที่คุณกำลังใช้งาน';
+$lang['db_transaction_failure'] = 'การดำเนินการ Transaction ล้มเหลว: ทำการ Rollback แล้ว';
+$lang['db_unable_to_drop'] = 'ไม่สามารถลบฐานข้อมูลที่ระบุได้';
+$lang['db_unsuported_feature'] = 'คุณสมบัติที่ไม่รองรับบนแพลตฟอร์มฐานข้อมูลที่คุณกำลังใช้งาน';
+$lang['db_unsuported_compression'] = 'รูปแบบการบีบอัดไฟล์ที่คุณเลือกไม่ได้รับการสนับสนุนโดยเซิร์ฟเวอร์ของคุณ';
+$lang['db_filepath_error'] = 'ไม่สามารถเขียนข้อมูลไปยังพาธไฟล์ที่คุณระบุได้';
+$lang['db_invalid_cache_path'] = 'พาธแคชที่คุณระบุไม่ถูกต้องหรือไม่สามารถเขียนได้';
+$lang['db_table_name_required'] = 'จำเป็นต้องระบุชื่อตารางสำหรับการดำเนินการนี้';
+$lang['db_column_name_required'] = 'จำเป็นต้องระบุชื่อคอลัมน์สำหรับการดำเนินการนี้';
+$lang['db_column_definition_required'] = 'จำเป็นต้องระบุคำจำกัดความของคอลัมน์สำหรับการดำเนินการนี้';
+$lang['db_unable_to_set_charset'] = 'ไม่สามารถตั้งค่าชุดตัวอักษรสำหรับการเชื่อมต่อไคลเอนต์: %s';
+$lang['db_error_heading'] = 'เกิดข้อผิดพลาดเกี่ยวกับฐานข้อมูล';
+
+/* End of file db_lang.php */
+/* Location: ./application/language/thai/db_lang.php */

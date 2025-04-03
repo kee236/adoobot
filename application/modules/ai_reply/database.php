6ae2ee425a95c349b414d96038938a29d576ffc7
kee236 <31869356+kee236@users.noreply.github.com>
2025-04-03 23:38:06 +0700

    Create database.php

---
+++ application/modules/ai_reply/database.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Addon Name: AI Integration
+Unique Name: ai_reply
+Modules:
+{
+   "340":{
+      "bulk_limit_enabled":"0",
+      "limit_enabled":"0",
+      "extra_text":"",
+      "module_name":"Bot - AI Reply"
+   }
+}
+Project ID: 67
+Version: 1.1
+Description: 
+INSERT INTO `add_ons` (`id`, `add_on_name`, `unique_name`, `version`, `installed_at`, `update_at`, `purchase_code`, `module_folder_name`, `project_id`) VALUES
+
+(4,'AI Integration', 'ai_reply', '1.1', '2024-01-15 12:00:00', '2024-01-15 12:00:00', '', 'ai_reply', 67);
+
+INSERT INTO `modules` (`id`, `module_name`, `add_ons_id`, `extra_text`, `limit_enabled`, `bulk_limit_enabled`, `deleted`) VALUES ('340', 'Bot - AI Reply', '4', '', '1', '0', '0');
+
+
+*/

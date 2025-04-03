1b0f9357ea9279e87028539729e852ea74b156db
kee236 <31869356+kee236@users.noreply.github.com>
2025-04-03 23:37:15 +0700

    Update Ai_reply.php

--- application/modules/ai_reply/controllers/Ai_reply.php
+++ application/modules/ai_reply/controllers/Ai_reply.php
@@ -12,11 +12,15 @@ Modules:
    }
 }
 Project ID: 67
-Addon URI: https://xerochat.com
-Author: Xerone IT
-Author URI: https://xeroneit.net
 Version: 1.1
 Description: 
+INSERT INTO `add_ons` (`id`, `add_on_name`, `unique_name`, `version`, `installed_at`, `update_at`, `purchase_code`, `module_folder_name`, `project_id`) VALUES
+
+(4,'AI Integration', 'ai_reply', '1.1', '2024-01-15 12:00:00', '2024-01-15 12:00:00', '', 'ai_reply', 67);
+
+INSERT INTO `modules` (`id`, `module_name`, `add_ons_id`, `extra_text`, `limit_enabled`, `bulk_limit_enabled`, `deleted`) VALUES ('340', 'Bot - AI Reply', '4', '', '1', '0', '0');
+
+
 */
 
 require_once("application/controllers/Home.php"); // loading home controller

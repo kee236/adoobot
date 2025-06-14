09ace68b2903bfab668d4b637488dcaa9564de63
kee236 <31869356+kee236@users.noreply.github.com>
2025-06-15 04:30:20 +0700

    Create language/Thai/visual_flow_builder.php

---
+++ application/modules/visual_flow_builder/language/Thai/visual_flow_builder.php
@@ -0,0 +1,9 @@
+<?php
+// เพิ่มเติมใน application/language/thai/common_lang.php หรือ visual_flow_builder_lang.php
+
+$lang['We\'re sorry but flow-builder doesn\'t work properly without JavaScript enabled. Please enable it to continue.'] = 'ขออภัย Flow Builder ไม่สามารถทำงานได้อย่างถูกต้องหากไม่ได้เปิดใช้งาน JavaScript กรุณาเปิดใช้งาน JavaScript เพื่อดำเนินการต่อ';
+
+// นอกจากนี้, ค่าเริ่มต้นของ 'page_name_or_insta_username' ก็ควรพิจารณาแปลด้วย หากจะแสดงเป็นค่าเริ่มต้น
+// (แต่ในโค้ดนี้ถูกจัดการเป็น string ตรงๆ ไม่ได้ผ่าน lang->line())
+// 'Page or account name' => 'ชื่อเพจหรือชื่อบัญชี'
+
